88c37bbd58a94de96aefca02304a68df922e1873
Suresh-a-26 <sureshayyanar2326@gmail.com>
2024-06-21 21:41:53 +0530

    first commit

---
+++ iterateResumeData.js
@@ -0,0 +1,31 @@
+let myResumeData = {
+
+    name : "suresh",
+    age  : 24,
+    currentLocation : "Chennai",
+    nativeLocation  : "Tuticorin",
+    email : "sureshayyanar2326@gmail.com",
+    phoneNumber : 7868845326,
+    education : {
+        college : "Saveetha Engineering College",
+        location: "Chennai",
+        degree  : "Bachelour of Engineering",
+        department : "Mechanical",
+        CGPA: 7.45
+    },
+    currentCompany: {
+        name : "Infosys",
+        location:"Chennai",
+        designation:"Senior Systems Engineer",
+        experience: "2.6 Years"
+    },
+    skillSets : {
+        programmingLanguages : ["HTML","CSS","JS","React"],
+        tools : ["Git","Github","JIRA","VS Code"]
+        },
+    languagesKnown : ["English","Tamil"],
+     hobbie: "reading books"
+
+
+}
+console.log(myResumeData);
